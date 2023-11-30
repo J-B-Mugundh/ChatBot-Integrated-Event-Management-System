c28ef9f5aa9c4372e706d08bbb390d7f87dafa3a
Mugundh <mugundhjb@gmail.com>
2023-11-30 10:45:49 +0530

    Updated Validate Participant

--- src/java/VaPa.java
+++ src/java/VaPa.java
@@ -5,6 +5,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;  
 import jakarta.servlet.http.HttpServletRequest;  
 import jakarta.servlet.http.HttpServletResponse;  
+import jakarta.servlet.http.HttpSession;  
 
 public class VaPa extends HttpServlet {  
     public void doPost(HttpServletRequest request, HttpServletResponse response)  
@@ -12,11 +13,21 @@ public class VaPa extends HttpServlet {
         response.setContentType("text/html");  
         PrintWriter out = response.getWriter();  
     
-        //Fetching data  from HTML form
+        // Fetching data from HTML form
         String n = request.getParameter("Pausername");  
         String p = request.getParameter("Papassword");  
           
         if (LoginDao.validate(n, p)) {  
+            // Creating a session
+            HttpSession session = request.getSession();
+            
+            // Set session timeout to 30 minutes (in seconds)
+            int sessionTimeout = 30 * 60;
+            session.setMaxInactiveInterval(sessionTimeout);
+            
+            // Storing user information in the session
+            session.setAttribute("username", n);
+            
             RequestDispatcher rd = request.getRequestDispatcher("ParticipantEvent.html");  
             rd.forward(request, response);  
         }  
